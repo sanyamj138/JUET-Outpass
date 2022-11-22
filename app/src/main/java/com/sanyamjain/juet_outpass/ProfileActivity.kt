1a838077d85923a704a5d12a42c4a7b3ab0de5b2
Sanyam Jain <sanyamj138@gmail.com>
2022-11-22 12:28:28 +0530

    Application activity connected to profile page

--- app/src/main/java/com/sanyamjain/juet_outpass/ProfileActivity.kt
+++ app/src/main/java/com/sanyamjain/juet_outpass/ProfileActivity.kt
@@ -1,11 +1,27 @@
 package com.sanyamjain.juet_outpass
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.common.SignInButton
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var submitButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        submitButton = findViewById(R.id.submitButton)
+
+        submitButton.setOnClickListener{
+            val intent = Intent(this, ApplicationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
