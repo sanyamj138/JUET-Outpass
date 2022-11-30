f94916db48824e2e064d7cb491adc1a779d32615
Sanyam Jain <sanyamj138@gmail.com>
2022-11-30 14:51:56 +0530

    App Design Updated

---
+++ app/src/main/java/com/sanyamjain/juet_outpass/GetStartedActivity.kt
@@ -0,0 +1,11 @@
+package com.sanyamjain.juet_outpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+    }
+}
