7b6e0d57c85e567ea80d0885227ecbc59e5b23ff
Sanyam Jain <sanyamj138@gmail.com>
2022-12-02 10:13:09 +0530

    Faculty Home Page Added

---
+++ app/src/main/java/com/sanyamjain/juet_outpass/FacultyProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.sanyamjain.juet_outpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacultyProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_faculty_profile)
+    }
+}
---
+++ app/src/main/java/com/sanyamjain/juet_outpass/HomeActivityFaculty.kt
@@ -0,0 +1,11 @@
+package com.sanyamjain.juet_outpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivityFaculty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_faculty)
+    }
+}
